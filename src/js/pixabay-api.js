2af1a7c0fc62967520e0a274ef1048e9ed5b48e0
anton81951 <145291034+anton81951@users.noreply.github.com>
2024-04-09 19:42:34 +0300

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,84 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import axios from "axios";
+import { displayImages } from "./render-functions";
+
+const apiKey = '9233093-942588744ee96c4f575017f3e';
+let currentPage=1;
+const resultsPerPage = 15;
+let totalHits = 0;
+let searchInputValue = '';
+
+async function pixabaySearch(isLoadMore) {
+    const input = document.querySelector('.input');
+    const currentSearchInput = input.value.trim();
+
+    const loadBtn = document.querySelector(".loadbtn");
+    loadBtn.style.display = 'none';
+
+    if (!isLoadMore) {
+        searchInputValue = currentSearchInput;
+    }
+
+    if (isLoadMore) {
+        currentPage++;
+    }
+
+    if (searchInputValue === "") {
+        showErrorToast("Empty input!");
+        return;
+    }
+
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'block';
+
+    let url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}`;
+
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        if (data.hits.length === 0) {
+            const errorMessage = isLoadMore ? "Sorry, there are no more images to load." : "Sorry, there are no images matching your search query. Please try again!";
+            showErrorToast(errorMessage);
+            document.querySelector(".form").reset();
+            loadBtn.style.display = 'none';
+        } else {
+            totalHits = data.totalHits;
+            const imagesToDisplay = data.hits.slice(0, resultsPerPage);
+            displayImages(imagesToDisplay, isLoadMore);
+            input.value = "";
+            if (totalHits <= currentPage * resultsPerPage) {
+                loadBtn.style.display = 'none';
+                if (totalHits > resultsPerPage && !isLoadMore) {
+                    showEndOfResultsMessage();
+                }
+            } else {
+                loadBtn.style.display = 'block';
+            }
+        }
+    } catch (error) {
+        console.error('axios problem', error);
+        showErrorToast('axios problem');
+        loadBtn.style.display = 'none';
+    } finally {
+        loader.style.display = 'none';
+    }
+}
+
+function showErrorToast(message) {
+    iziToast.error({
+        title: 'Error',
+        message: message,
+        position: 'topCenter'
+    });
+}
+
+function showEndOfResultsMessage() {
+    iziToast.info({
+        title: 'Info',
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topCenter'
+    });
+}
+
+export { pixabaySearch, showErrorToast, showEndOfResultsMessage, searchInputValue };
---
+++ src/main.js
@@ -0,0 +1,44 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from "axios";
+
+import { pixabaySearch, showErrorToast, showEndOfResultsMessage } from "./js/pixabay-api";
+
+const gallery = document.querySelector('.gallery');
+const loader = document.createElement('div');
+
+loader.style.display = 'none';
+loader.classList.add('loader');
+document.body.appendChild(loader);
+
+const loadBtn = document.querySelector(".loadbtn");
+loadBtn.style.display = 'none';
+
+document.querySelector(".form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    loader.style.display = 'block';
+    gallery.innerHTML = '';
+
+    try {
+        await pixabaySearch(false);
+    } catch (error) {
+        console.error('fetching error', error);
+        showErrorToast('fetching error');
+    } finally {
+        loader.style.display = 'none';
+    }
+});
+
+document.querySelector(".loadbtn").addEventListener("click", async () => {
+    loader.style.display = 'block';
+    await pixabaySearch(true);
+    loader.style.display = 'none';
+
+    const galleryHeight = gallery.offsetHeight;
+    window.scrollBy({
+        top: galleryHeight / 2,
+        behavior: 'smooth'
+    });
+});
